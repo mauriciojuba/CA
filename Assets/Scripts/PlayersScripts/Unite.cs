320d8066dc1cafed1b53fe467d8e46fc8cef16ab
mauriciojuba <mauriciojuba@outlook.com>
2016-06-04 06:36:43 -0300

    Unite...mtf

--- Assets/Scripts/PlayersScripts/Unite.cs
+++ Assets/Scripts/PlayersScripts/Unite.cs
@@ -15,21 +15,15 @@ public class Unite : MonoBehaviour {
     {
 		if (InputManager.players == 2) {
 			if (InputManager.YButton () && closeEnough && everybodyOk && !attach) {
-//				skill = true;
-				//this.GetComponent<SwitchPlayer>().controlling = "Player2";
+				skill = true;
 				this.GetComponent<Animator> ().SetBool ("Unite", true);
 				this.GetComponent<CapsuleCollider> ().enabled = false;
-
-				//this.transform.SetParent(CampSpawner);
 
 				attach = true;
 			}
 		} else {
         	if ((this.GetComponent<SwitchPlayer> ().controlling == "Player1") && InputManager.YButton () && closeEnough && everybodyOk && !attach) {
 				skill = true;
-                //this.GetComponent<SwitchPlayer>().controlling = "Player2";
-                //				this.GetComponent<Animator> ().SetBool ("OnGround", false);
-                //				this.GetComponent<Animator> ().enabled = false;
                 this.GetComponent<Animator>().SetBool("Unite", true);
 				this.GetComponent<CapsuleCollider> ().enabled = false;
             
@@ -46,12 +40,9 @@ public class Unite : MonoBehaviour {
 			Vector3 heightCorrectedPoint = new Vector3 (Turnip.transform.position.x, this.transform.position.y, Turnip.transform.position.z);
 			this.transform.LookAt (heightCorrectedPoint);
 			if (InputManager.YButton () && count >= 3f) {
-                //transform.position = new Vector3 (transform.position.x, transform.position.y + 0.5f, transform.position.z);
                 this.transform.position = CampDump.position;
 				this.GetComponent<CapsuleCollider> ().enabled = true;
                 this.GetComponent<Animator>().SetBool("Unite", false);
-                //				this.GetComponent<ThirdPersonUserControlCamponesa> ().enabled = true;
-                //				this.GetComponent<ThirdPersonUserControlCamponesa> ().m_Jump = true;
 
                 attach = false;
 				count = 0;
