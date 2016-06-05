9464f39ae9c9d607ecafa6bfa33a65e8b09fb827
hparoli <henrique.paroli@gmail.com>
2016-06-05 20:27:29 -0300

    Sons Cutscene, Indicador Menu Circular - OK

---
+++ Assets/CutsceneInicialMusica.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutsceneInicialMusica : MonoBehaviour {
+
+	public AudioSource audio;
+	public AudioClip intro, loop;
+	public bool loopPlaying;
+
+	// Use this for initialization
+	void Start () {
+		audio.clip = intro;
+		audio.loop = false;
+		audio.Play ();
+		loopPlaying = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!audio.isPlaying && !loopPlaying) {
+			audio.clip = loop;
+			audio.loop = true;
+			audio.Play ();
+			loopPlaying = true;
+		}
+	}
+}
