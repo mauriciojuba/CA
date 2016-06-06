f957273acbd1f4a4630a7f464254b6c92fd636c0
mauriciojuba <mauriciojuba@outlook.com>
2016-06-06 01:53:53 -0300

    Creditos

---
+++ Assets/Scripts/Utils/preventLimbo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class preventLimbo : MonoBehaviour {
+
+    public Transform camp;
+
+	
+	void Update () {
+        if (transform.position.z < camp.position.z)
+        {
+            if (camp.position.y <= transform.position.y)
+            {
+                camp.position = new Vector3(camp.position.x, camp.position.y + 3f, camp.position.z);
+            }
+        }
+	}
+}
